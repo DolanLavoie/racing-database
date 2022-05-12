3ec13219ff8f49b194c119e5952d2f5670716897
DolanLavoie <85038406+DolanLavoie@users.noreply.github.com>
2022-05-12 13:36:37 -0600

    Upload race, season, and track

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const port = 3000
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-
+const { DateTimeOffset } = require('mssql');
 const dao = require('./dao.js');
-const e = require('express');
 
 app.use(cors());
 app.use(express.json());
@@ -19,64 +18,39 @@ app.get('/', (req, res) => {
   
 })
 
-
-
-app.get("/post", async (req, res) => {
-  var armyName = req.query.armyName;
-  var warlordName = req.query.warlordName;
-  var numInfantry = req.query.numInfantry;
-  var numCavalry = req.query.numCavalry;
-  var AF = req.query.AF;
-  if(AF == "yes"){
-    AF = 1;
-  }else{
-    AF = 0;
-  }
-  await dao.post(armyName, warlordName, numInfantry, numCavalry, AF);
+app.get('/uploadCar', async (req, res) => {
+  var name = req.query.name;
+  var topSpeed = req.query.topSpeed;
+  var breaking = req.query.breaking;
+  var turning = req.query.turning;
+  await dao.uploadCar(name, topSpeed, breaking, turning);
   res.sendStatus(200);
 });
 
-app.get("/getArmies", async (req, res) => {
-  var armies = await dao.getArmies();
-  res.send(armies);
-});
-
-app.get("/getBattles", async (req, res) => {
-  var battles = await dao.getBattles();
-  res.send(battles);
-});
-
-app.get("/battle", async (req, res) => {
-  var army1 = req.query.army1;
-  var army2 = req.query.army2;
+app.get('/uploadTrack', async (req, res) => {
   var name = req.query.name;
-  var army1Stats = await dao.getArmyStats(army1);
-  var army2Stats = await dao.getArmyStats(army2);
-  var army1Power = calculatePower(army1Stats.recordset[0].infantry, army1Stats.recordset[0].cavalry, army1Stats.recordset[0].AF);
-  var army2Power = calculatePower(army2Stats.recordset[0].infantry, army2Stats.recordset[0].cavalry, army2Stats.recordset[0].AF);
-  var winner;
-  if(army1Power > army2Power){
-    winner = army1;
-  }else if(army2Power > army1Power){
-    winner = army2;
-  }else{
-    winner = "Tie";
-  }
-  await dao.updateBattleTable(name, army1, army2, army1Power, army2Power, winner);
-  res.send(army1Power + " vs. " + army2Power + "<br>" + winner);
+  var topSpeed = req.query.topSpeed;
+  var breaking = req.query.breaking;
+  var turning = req.query.turning;
+  await dao.uploadTrack(name, topSpeed, breaking, turning);
+  res.sendStatus(200);
 });
 
-function calculatePower(infantry, cavalry, AF){
-  var power = random(infantry) + (random(cavalry) * 3);
-  if(AF == true){
-    power *= 2;
-  }
-  return power;
-}
+app.get('/uploadSeason', async (req, res) => {
+  var name = req.query.name;
+  var winner = req.query.winner;
+  await dao.uploadSeason(name, winner);
+  res.sendStatus(200);
+});
 
-function random(max){
-  return Math.floor(Math.random() * max);
-}
+app.get('/uploadRace', async (req, res) => {
+  var name = req.query.name;
+  var track = req.query.racedAt;
+  var winner = req.query.winner;
+  var season = req.query.season;
+  await dao.uploadRace(name, track, winner, season);
+  res.sendStatus(200);
+})
 
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
--- dao.js
+++ dao.js
@@ -1,58 +1,45 @@
 const sql = require('mssql');
 const connection = 'Server=localhost,1433;Database=ArmyBuilder;User Id=test;Password=password;Encrypt=false';
 
-async function post(armyName, warlordName, numInfantry, numCavalry, AF){
+async function uploadCar(name, topSpeed, braking, turning){
     try{
         await sql.connect(connection);
-        const result = await sql.query`INSERT INTO Army VALUES(${armyName}, ${warlordName}, ${numInfantry}, ${numCavalry}, ${AF})`;
+        await sql.query`INSERT INTO Cars VALUES(${name}, ${topSpeed}, ${braking}, ${turning})`;
     } catch (err) {
         console.log(err);
     }
 }
 
-async function getArmies(){
+async function uploadTrack(name, topSpeed, breaking, turning){
     try{
         await sql.connect(connection);
-        const result = await sql.query`SELECT * FROM Army`;
-        return result;
+        await sql.query`INSERT INTO Tracks VALUES(${name}, ${topSpeed}, ${breaking}, ${turning})`;
     } catch (err) {
         console.log(err);
     }
 }
 
-async function getArmyStats(army){
+async function uploadSeason(name, winner){
     try{
         await sql.connect(connection);
-        const result = await sql.query`SELECT * FROM Army WHERE armyName = ${army}`;
-        return result;
-    } catch (err){
+        await sql.query`INSERT INTO Season VALUES(${name}, ${winner})`;
+    } catch (err) {
         console.log(err);
     }
 }
 
-async function updateBattleTable(battleName, army1, army2, army1Strength, army2Strength, winner){
+async function uploadRace(name, track, winner, season){
     try{
         await sql.connect(connection);
-        const result = await sql.query`INSERT INTO Battles VALUES(${battleName}, ${army1}, ${army2}, ${army1Strength}, ${army2Strength}, ${winner})`
-    } catch (err){
-        console.log(err);
-    }
-}
-
-async function getBattles(){
-    try{
-        await sql.connect(connection);
-        const result = await sql.query`SELECT * FROM Battles`;
-        return result;
-    } catch (err){
+        await sql.query`INSERT INTO Race VALUES(${name}, ${track}, ${winner}, ${season})`;
+    } catch (err) {
         console.log(err);
     }
 }
 
 module.exports = {
-    post,
-    getArmies,
-    getArmyStats,
-    updateBattleTable,
-    getBattles
-}
+    uploadCar,
+    uploadTrack,
+    uploadSeason,
+    uploadRace
+};
